8e2c8f013dad4bdf07918929ba5f827d5f9518e3
Zach Hewitt <zach@zachs-mbp.home>
2019-09-18 17:18:38 -0600

    added more specs/edited controller/edited routes

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,12 +1,28 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
-  def index
-    @gram = Gram.all
-  end
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
 
+    @gram.update_attributes(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+  
   def new
     @gram = Gram.new
+  end
+
+  def index
+  end
+
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
   end
 
   def create
@@ -18,11 +34,18 @@ class GramsController < ApplicationController
     end
   end
 
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end
 
-
-  private 
+  private
 
   def gram_params
     params.require(:gram).permit(:message)
+  end
+
+  def render_not_found
+    render plain: 'Not found', status: :not_found
   end
 end
